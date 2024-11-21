3723f538b157dce9e875b002c1a7ba3afd9c5ccc
jaket <thibaultjake95@gmail.com>
2024-11-20 19:46:53 -0500

    cleaning some stuff up

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
+//defult color
 let color = "black";
 
 //create div using js //putting grid squares in the container div
-function populateContainer(size){
-    let container = document.querySelector(".container");
-    (container.querySelectorAll("div")).forEach((div) => div.remove());
-    container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+function populateContainer(size) {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-    let amount = size * size;
-    for (let i = 0; i < amount; i++) {
-      let square = document.createElement("div");
-      square.classList.add("cell");
-      container.style.backgroundColor = "#e0e0e0";
-      //“hover” effect
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.classList.add("cell");
+    container.style.backgroundColor = "#e0e0e0";
+   
+//hover effect
       square.addEventListener("mouseover", colorSquare);
-      container.insertAdjacentElement("beforeend", square);
+      container.appendChild(square);
     }
-  }
+  }  
 
   //16x16 grid of square divs
   populateContainer(16);
 
-  // input asking for new grid
+// error if conditions arent met
   function changeSize(input) {
-    input >= 2 && input <= 100 ? populateContainer(input) : console.log("input error! to many or to few squares");
+    input >= 2 && input <= 100 ? populateContainer(input) : console.log("Input error! Please provide a number between 2 and 100.");
   };
-
+//random colors
   function colorSquare(){
     color === "random" ? this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)` : this.style.backgroundColor = color;
   }
-
+// change colors black,gray,rainbow
   function changeColor(choice){
     color = choice;
   }
+// reset background
   function resetContainer() {
     let container = document.querySelector(".container");
     let squares = container.querySelectorAll("div");
